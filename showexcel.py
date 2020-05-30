c29a3ee6a4bc441187c655ef40cc91c4abe8cabd
Kacper Szymański <k4cpers9@gmail.com>
2020-05-30 12:13:51 +0200

    added comments

--- showexcel.py
+++ showexcel.py
@@ -1,6 +1,17 @@
 from win32 import win32gui
+'''
+set of functions to make excel visible if opened
+
+returns:
+    - true if excel with opened program file is opened
+    - false if excel with opened program file in closed
+
+note to self: filename must be a filename and not filepath.
+'''
+
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+
 def show_excel_window(filename):
     top_windows = []
     win32gui.EnumWindows(windowEnumerationHandler, top_windows)
